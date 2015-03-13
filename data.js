9a6ceb3800535ca2a5609284fd7508586133bc07
David Ok <davidok@gmail.com>
2015-03-13 18:01:35 -0400

    Added information to data file.

---
+++ data.js
@@ -0,0 +1,9 @@
+//array setup from two-line Google Charts example: https://developers.google.com/chart/interactive/docs/gallery/linechart
+var ebolaData = {
+      data.addRows([
+        ["2014-08-29", 3052, 1546],   ["2014-09-28", 7157, 3330],  ["2014-10-31", 13567, 4951],
+        ["2014-11-26", 15935, 5689],  ["2014-12-31", 20206, 7904],  ["2015-01-25", 22092, 8810],
+        ["2015-02-22", 23729, 9604]
+      ]);
+  
+}
