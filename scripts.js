1cb4b9ebee82f6cf37604c83e0eb3c90c05f05e8
David Ok <davidok@gmail.com>
2015-03-13 19:21:27 -0400

    Created for loop.

--- scripts.js
+++ scripts.js
@@ -7,16 +7,64 @@ google.load('visualization', '1.0', {
 google.setOnLoadCallback(drawChart);
 
 // Callback that creates and populates a data table,
-// instantiates the chart, passes in the data and
+// instantiates the pie chart, passes in the data and
 // draws it.
-    function drawChart() {
-    	
-    	var bigArray = [];
-    	
+function drawChart() {
 
-      var data = new google.visualization.DataTable();
-      data.addColumn('date', 'Date');
-      data.addColumn('number', 'Cases');
-      data.addColumn('number', 'Deaths');
+	var bigArray = [];
 
-      data.addRows([
+	for (var i = 0; i < ebolaData.length; i++) {
+
+		var smallerArray = [];
+
+		var myData = ebolaData[i];
+
+		smallerArray.push(myData.date);
+		smallerArray.push(myData.cases);
+		smallerArray.push(myData.deaths);
+
+		bigArray.push(smallerArray);
+
+	}//end of for loop
+
+	function drawChart() {
+
+		var data = new google.visualization.DataTable();
+		data.addColumn('string', 'Date');
+		data.addColumn('number', 'Cases');
+		data.addColumn('number', 'Deaths');
+
+		data.addColumn({
+			type : 'string',
+			role : 'style'
+		})
+
+		data.addRows(bigArray);
+
+		var numberFormatter = new google.visualization.NumberFormat({
+			fractionDigits : 0
+		});
+		numberFormatter.format(data, 1);
+
+		var options = {
+			width : 800,
+			height : 500,
+			hAxis : {
+				title : 'Date'
+			},
+			vAxis : {
+				title : 'Incidences'
+			},
+			series : {
+				1 : {
+					curveType : 'function'
+				}
+			}
+		};
+
+		var chart = new google.visualization.LineChart(document.getElementById('ex2'));
+
+		chart.draw(data, options);
+	}
+
+}
